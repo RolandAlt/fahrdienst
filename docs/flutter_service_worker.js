1f15dc2ca7265a6e945e5c7390cad4e82982b8b3
Roland Alt <roland.alt@gmx.de>
2025-11-28 18:31:32 +0100

    Rebuild web with correct base-href

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "95c5f45d1ece4d1d5c19f689381eeb64
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2e87e85dd653625af2d109f1e0a2521a",
+"flutter_bootstrap.js": "31c7650a6f86423fcffbb7e0e05f02be",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b546e8c5080c8aaddbeb40d1ee6b83bf",
 "/": "b546e8c5080c8aaddbeb40d1ee6b83bf",
-"main.dart.js": "e31e52ef934e5fa3bb6dabf945d2cdd5",
+"main.dart.js": "c7029c55d92351fb50b03191a38078a4",
 "manifest.json": "75fd670769c3413d905103124228700c",
 "version.json": "3ea3ce6f614fca3c75320b10352d9857"};
 // The application shell files that are downloaded before a service worker can
