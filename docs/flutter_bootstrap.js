7c4fc788834942f5cd7b3392f65ae86071b210a0
Roland Alt <roland.alt@gmx.de>
2025-11-29 15:42:30 +0100

    Rebuild web with correct base-href

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3354913292"
+    serviceWorkerVersion: "4024841903"
   }
 });
